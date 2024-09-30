c483bc5cdf38a2b954ba2915c014f31aa48b9f32
Drylian <109999325+drylian@users.noreply.github.com>
2024-09-30 04:43:23 -0400

    Remove esbuild-dts-path-alias (Bug)
    Remove esbuild-dts-path-alias for more bugs in build, repair cjs (sorry qwq)

--- tools/build.ts
+++ tools/build.ts
@@ -1,57 +1,50 @@
 import esbuild from "esbuild";
 import fs from "node:fs";
 import { dTSPathAliasPlugin } from "esbuild-dts-path-alias";
+import { execSync } from "node:child_process";
 
 console.log("Building Esm resources...");
 await esbuild.build({
-    entryPoints: ["./src/Loggings.ts"],
-    outfile: "./dist/Loggings.mjs",
-    bundle: true,
-    minify: true,
-    format: "esm",
-    target: ["esnext"],
-    platform: "node",
-    plugins:[
-        dTSPathAliasPlugin({
-            tsconfigPath: "./tsconfig.json",
-            outputPath: "./types",
-            debug: true,
-        }),
-    ]
+  entryPoints: ["./src/Loggings.ts"],
+  outfile: "./loggings.mjs",
+  bundle: true,
+  minify: true,
+  format: "esm",
+  target: ["esnext"],
+  platform: "node",
 });
-
+console.log("Building Types resources...");
+execSync("tsc --project ./tsconfig.json");
 console.log("Building Cjs resources...");
 await esbuild.build({
-    entryPoints: ["./src/Loggings.ts"],
-    outfile: "./dist/Loggings.cjs",
-    bundle: true,
-    minify: true,
-    minifySyntax: true,
-    format: "cjs",
-    target: ["node14"],
-    platform: "node",
-    plugins: [
-        {
-        name: "RenameImport",
-        setup(build) {
-            build.onLoad({ filter: /\.ts$/ }, async (args) => {
-                const text = (await fs.promises.readFile(args.path, "utf8"))
-                    .replace(
-                        /node:console/g,
-                        "console"
-                    )
-                    .replace("const require = Module.createRequire(import.meta.url);", "")
-                    .replace(
-                        /node:util/g,
-                        "util"
-                    );
-                return {
-                    contents: text,
-                    loader: "ts",
-                };
-            });
-        },
-    }],
+  entryPoints: ["./src/Loggings.ts"],
+  outfile: "./loggings.cjs",
+  bundle: true,
+  minify: true,
+  minifySyntax: true,
+  format: "cjs",
+  target: ["node14"],
+  platform: "node",
+  plugins: [
+    {
+      name: "RenameImport",
+      setup(build) {
+        build.onLoad({ filter: /\.ts$/ }, async (args) => {
+          const text = (await fs.promises.readFile(args.path, "utf8"))
+            .replace(/node:console/g, "console")
+            .replace(
+              "const require = Module.createRequire(import.meta.url);",
+              "",
+            )
+            .replace(/node:util/g, "util");
+          return {
+            contents: text,
+            loader: "ts",
+          };
+        });
+      },
+    },
+  ],
 });
 
 // console.log("Building Cdn mjs resources...");
