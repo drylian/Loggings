ccbf5100b1d1ab4f7127eb3dd79a406ce7430d45
Drylian <109999325+drylian@users.noreply.github.com>
2025-04-13 02:18:35 -0400

    repair infinity size in use

--- src/libs/inspect.ts
+++ src/libs/inspect.ts
@@ -2,19 +2,22 @@ import { Runtime, runtime } from "./utils";
 
 let _inspect = (msg: string | boolean | object | number, nocolor: boolean = false) => {
     // @ts-expect-error in browser inspect is global
-    return inspect(msg, { colors: !nocolor });
+    return inspect(msg, { colors: !nocolor, depth: 3 });
 }
 
 switch (runtime) {
     case Runtime.Node: {
         switch (true) {
             case typeof __filename == "undefined": {
-                _inspect = (msg, nocolor = false) => import("node:util").then(({ inspect }) => inspect(msg, { colors: !nocolor }));
+                _inspect = (msg, nocolor = false) => 
+                    import("node:util").then(({ inspect }) => 
+                        inspect(msg, { colors: !nocolor, depth: 3 }));
                 break;
             }
             default: {
                 const inspect = require("node:util");
-                _inspect = (msg, nocolor = false) => inspect(msg, { colors: !nocolor });
+                _inspect = (msg, nocolor = false) => 
+                    inspect(msg, { colors: !nocolor, depth: 3 });
                 break;
             }
         }
@@ -22,12 +25,12 @@ switch (runtime) {
     }
     case Runtime.Bun: {
         // @ts-expect-error @types/Bun not installed
-        _inspect = (msg, nocolor = false) => Bun.inspect(msg, { colors: !nocolor });
+        _inspect = (msg, nocolor = false) => Bun.inspect(msg, { colors: !nocolor, depth: 3 });
         break;
     }
     case Runtime.Deno: {
         // @ts-expect-error @types/Deno not installed
-        _inspect = (msg, nocolor = false) => Deno.inspect(msg, { colors: !nocolor });
+        _inspect = (msg, nocolor = false) => Deno.inspect(msg, { colors: !nocolor, depth: 3 });
         break;
     }
 }
